ea719feb02da3a6bafe5ff8028051650dc0de63c
martin-lukas <martinlukas24@protonmail.com>
2021-03-05 18:22:30 +0100

    Restructure project and fix build issues

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,12 +1,10 @@
 var React = require('react');
-var Greet = require('./Greet.jsx');
 
 var App = React.createClass({
     render: function () {
         return (
             <div>
                 <h1>React.js</h1>
-                <Greet />
             </div>
         );
     }
