55b925481bcf130c5171647c53f0380dcc24d1c3
galasd <dave.galas@gmail.com>
2021-03-11 14:27:19 +0100

    Form validation

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -7,9 +7,11 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Signup/>
-        </div>
+        <React.StrictMode>
+            <div className="App">
+                <Signup/>
+            </div>
+        </React.StrictMode>
     );
 }
 
